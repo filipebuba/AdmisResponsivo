c5298f0c2f2856058ebdf0435783a908f8f33158
FILIPE BUBA <nhadafilipe@gmail.com>
2022-04-11 03:21:22 -0300

    Carregando_produtos
    Carregando produtos no js

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ themeToggler.addEventListener("click", () => {
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+// fill orders in tabela
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.ProductName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'Pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Details</td>
+                      `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    { 
+        ProductName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    { 
+        ProductName: 'LAVANDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined' 
+    },
+    { 
+        ProductName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        ProductName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    { 
+        ProductName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+];
